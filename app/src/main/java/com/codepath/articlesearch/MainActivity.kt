1090f0aa1042368cae7327d23200950a95f141aa
nilmo-contreras <noc2@njit.edu>
2023-03-02 17:01:10 -0500

    Completed Lab 6

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -2,7 +2,9 @@ package com.codepath.articlesearch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.codepath.articlesearch.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 
 fun createJson() = Json {
@@ -22,5 +24,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val fragmentManager = supportFragmentManager
+
+        val booksFragment: Fragment = BestSellerBooksFragment()
+        val articleFragment: Fragment = ArticleListFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_books -> fragment = booksFragment
+                R.id.nav_articles -> fragment = articleFragment
+            }
+            fragmentManager.beginTransaction().replace(R.id.article_frame_layout, fragment).commit()
+            true
+        }
+
+        // replaceFragment(ArticleListFragment())
     }
+
+//    private fun replaceFragment(articleListFragment: ArticleListFragment) {
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
+//        fragmentTransaction.commit()
+//    }
 }
